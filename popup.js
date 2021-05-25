14ca43835a9a231e95714ab5bf0d2f9d3b654fd2
Mehul Gohel <mehul2108@gmail.com>
2021-05-25 22:36:44 +0530

    Update popup.js
    support 45+

--- popup.js
+++ popup.js
@@ -1,21 +1,37 @@
-let StartButton = document.getElementById('startProcess');
-
-StartButton.onclick = function (element) {
-
-    chrome.tabs.query({ active: true }, function (tabs) {
-        var tab = tabs[0];
-
-        chrome.tabs.sendMessage(tab.id, { "message": "start" });
-    });
+document.getElementById('startProcessEighteen').onclick = function (element) {
+    buttonclick('start', 18);
 };
 
-let StopButton = document.getElementById('stopProcess');
+document.getElementById('stopProcessEighteen').onclick = function (element) {
+    buttonclick('stop', 18);
+};
 
-StopButton.onclick = function (element) {
+document.getElementById('startProcessFortyfive').onclick = function (element) {
+    buttonclick('start', 45);
+};
 
+document.getElementById('stopProcessFortyfive').onclick = function (element) {
+    buttonclick('stop', 45);
+};
+
+document.getElementById('ageEighteen').onclick = function (element) {
+    document.getElementById('eighteen').style.display = "block";
+    document.getElementById('fortyfive').style.display = "none";
+};
+
+document.getElementById('ageFortyfive').onclick = function (element) {
+    document.getElementById('eighteen').style.display = "none";
+    document.getElementById('fortyfive').style.display = "block";
+};
+
+const buttonclick = (action, age) => {
     chrome.tabs.query({ active: true }, function (tabs) {
         var tab = tabs[0];
 
-        chrome.tabs.sendMessage(tab.id, { "message": "stop" });
+        chrome.tabs.sendMessage(tab.id, { "message": action, "age": age });
     });
+}
+
+document.getElementById('startProcessEighteen').onclick = function (element) {
+    buttonclick('start', 18);
 };
